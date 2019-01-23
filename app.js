eb6351bb812c96a0f068f9ba48f7f50540b8346e
Nick Riemondi <nick@riemondi.com>
2019-01-23 15:12:25 +0000

    I slept and realized I was an idiot and just needed to add padding-top to all of the tags

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
 $('body').scrollspy({ target: '#navbar' });
-
-// Offset anchor links to prevent fixed nav from overlapping text
-
-function offsetAnchor() {
-  if (location.hash.length !== 0) {
-    window.scrollTo(window.scrollX, window.scrollY + 20);
-  }
-}
-
-$(document).on('click', 'a[href^="#tag-"]', function(event) {
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
-});
-
-window.setTimeout(offsetAnchor, 0);

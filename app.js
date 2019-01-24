bb0c05a9c53f79be1bea1c8960228211f5c14375
Nick Riemondi <nick@riemondi.com>
2019-01-24 05:16:25 +0000

    added smooth scrolling to nav links

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 $('body').scrollspy({ target: '#navbar' });
+
+// Animate navbar clicks
+$('#navbar a').on('click', function(e) {
+  e.preventDefault();
+  // Empty Hash means they clicked my name, scroll back to the top
+  if (this.hash === '') {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+  }
+  // Otherwise scroll to the clicked hash
+  else {
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      500,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
